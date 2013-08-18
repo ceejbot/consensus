bf8ca1fcd01460545cd03fa648a41a97e8c891ae
C J Silverio <ceejceej@gmail.com>
2013-08-17 17:40:45 -0700

    Placeholder for the api implementation.

---
+++ routes/api.js
@@ -0,0 +1,23 @@
+// API implementation here.
+
+exports.person = function(request, response)
+{
+
+};
+
+exports.people = function(request, response)
+{
+
+};
+
+exports.agenda = function(request, response)
+{
+
+};
+
+exports.agendas = function(request, response)
+{
+
+};
+
+// etc etc
